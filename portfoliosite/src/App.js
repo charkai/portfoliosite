114d4f3a4740194319bbc8bc1eeab8d21549147b
soft3888_m10_03_p11-admin <ckai8237@uni.sydney.edu.au>
2024-11-05 21:12:10 +1100

    starting on timeline

--- portfoliosite/src/App.js
+++ portfoliosite/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './shared-components/Navbar';
 import Home from './Home';
 import About from './About';
+import Experience from './Experience';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/experience" element={<Experience/>} />
             </Routes>
           </Router>
       </header>
--- portfoliosite/src/shared-components/Navbar.js
+++ portfoliosite/src/shared-components/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
                         <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Education</a>
                     </li>
                     <li>
-                        <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Experience</a>
+                        <Link to="/experience" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Experience</Link>
                     </li>
                     <li>
                         <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Projects</a>
