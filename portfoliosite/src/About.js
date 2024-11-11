1df70fb5e7ff4c2c9ffbe2586f9f01d2df654f2b
soft3888_m10_03_p11-admin <ckai8237@uni.sydney.edu.au>
2024-11-11 17:37:58 +1100

    improving grid

--- portfoliosite/src/About.js
+++ portfoliosite/src/About.js
@@ -15,18 +15,40 @@ const About = () => {
         <div className="bg-gray-100 w-full flex flex-col items-center px-10 md:px-40 pt-40">
             {/* ABOUT ME CARD */}
 
-            <div className="bg-white border border-gray-200 rounded-lg shadow flex">
+            <div className="bg-white border border-gray-200 rounded-lg shadow flex mb-10">
                 <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-80 md:rounded-none md:rounded-s-lg" src={Headshot} alt="photo"></img>
                 <div className="flex flex-col justify-between p-4 leading-normal">
                     {/* <h3 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-800 md:text-5xl lg:text-6xl">About Me</h3> */}
                     <p className="mb-3 font-normal text-xl text-gray-700">Hi, I'm Charlie! I'm a full-time student with a passion for escape rooms, trivia nights, history podcasts, Ultimate Frisbee, Football and Formula 1. In my engineering degree and career thus far, I have gained a strong foundation with a range of computer science fundamentals - including object-oriented programming with Java, memory management in C, databases with SQL, and front-end development skills with JQuery, HTML and Laravel, which I was lucky enough to learn during my internship at revolutioniseSport. In the Psychology component of my degree, I have gained exposure to several disciplines. I am particularly fascinated by neuroscience and cognition, and I would love to one day work in a field that combines the best elements of both of my courses, such as AI or cybersecurity.</p>
                 </div>
             </div>
+            
+
+            <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl pb-10">Skills</h1>
 
 
-            
-            <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl pt-10">Skills</h1>
-            
+            <div class="grid grid-cols-2 md:grid-cols-2 gap-4 pb-20">
+
+                
+                <div className="bg-white border border-gray-200 rounded-lg shadow flex w-full mb-4">
+                    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-80 md:rounded-none md:rounded-s-lg" src={ReactSkills} alt="photo"></img>
+                    <div className="flex flex-col justify-between p-4 leading-normal">
+                        <h3 className="mb-4 text-xl font-extrabold leading-none tracking-tight text-gray-800">React and Modern Frameworks</h3>
+                        <p className="mb-3 font-normal text-xl text-gray-700">I slay at react</p>
+                    </div>
+                </div>
+
+
+                <div className="bg-white border border-gray-200 rounded-lg shadow flex w-full mb-4">
+                    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-80 md:rounded-none md:rounded-s-lg" src={PresentationSkills} alt="photo"></img>
+                    <div className="flex flex-col justify-between p-4 leading-normal">
+                        <h3 className="mb-4 text-xl font-extrabold leading-none tracking-tight text-gray-800">Communication Skills</h3>
+                        <p className="mb-3 font-normal text-xl text-gray-700">I love to yap</p>
+                    </div>
+                </div>
+
+            </div>
+
 
 
 
