dedd381d5ef5625c476445a0961d69f148c9ad98
soft3888_m10_03_p11-admin <ckai8237@uni.sydney.edu.au>
2025-02-26 11:46:38 +1100

    adding alternative about page

--- portfoliosite/src/App.js
+++ portfoliosite/src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import Navbar from './shared-components/Navbar';
 import Home from './Home';
 import About from './About';
+import Altabout from './AltAbout'
 import Experience from './Experience';
 import Projects from './Projects';
 import Education from './Education';
+import AltAbout from './AltAbout';
 
 function App() {
   return (
@@ -16,7 +18,7 @@ function App() {
             <Navbar/>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
+              <Route path="/about" element={<AltAbout />} />
               <Route path="/experience" element={<Experience/>} />
               <Route path="/projects" element={<Projects/>} />
               <Route path="/education" element={<Education/>} />
