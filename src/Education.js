5da57d2e4c5b741325a5008609933af49169e58f
soft3888_m10_03_p11-admin <ckai8237@uni.sydney.edu.au>
2025-03-04 19:00:06 +1100

    adding awards section

--- src/Education.js
+++ src/Education.js
@@ -51,30 +51,37 @@ const Education = () => {
     const awards = [
         {
             "awardName": "Chancellor's Award",
+            "awardDetails": "",
             "awardDate": "2021-2025"
         },
         {
             "awardName": "Dean's List of Excellence in Academic Performance (Faculty of Engineering)",
+            "awardDetails": "",
             "awardDate": "2022"
         },
         {
             "awardName": "Vice Chancellor's Global Mobility Merit scholarship",
+            "awardDetails": "",
             "awardDate": "2023"
         },
         {
             "awardName": "UniSport Nationals Division 1 Gold Medalist",
+            "awardDetails": "",
             "awardDate": "2022"
         },
         {
             "awardName": "Old Girls' Union Prize for Scholarship, Sportsmanship and Leadership",
+            "awardDetails": "",
             "awardDate": "2020"
         },
         {
             "awardName": "Dux of HSC Candidature",
+            "awardDetails": "",
             "awardDate": "2020"
         },
         {
             "awardName": "Prescott Medal for Highest Tertiary Admissions Rank (99.95)",
+            "awardDetails": "",
             "awardDate": "2020"
         },
         
@@ -111,21 +118,116 @@ const Education = () => {
             {/* AWARDS */}
 
             <h1 class="mb-4 mt-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Awards</h1>
-                <ul class="grid grid-cols-1 md:grid-cols-3 gap-4 pb-20 pt-20 space-y-4 text-gray-500 dark:text-gray-400">
+
+                <ul class="px-10 mb-10 bg-white border border-gray-200 rounded-lg shadow divide-y divide-gray-200 dark:divide-gray-700 max-w-full">
                     {awards.map((award, index) => (
                         <Award 
                         awardName={award.awardName}
+                        awardDetails={award.awardDetails}
                         awardDate={award.awardDate}
                         />
                     ))}
                 </ul>
             
 
-            
-
         </animated.div>
     )
 }
+
+
+{/* <ul class="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
+   <li class="pb-3 sm:pb-4">
+      <div class="flex items-center space-x-4 rtl:space-x-reverse">
+         <div class="shrink-0">
+            <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image">
+         </div>
+         <div class="flex-1 min-w-0">
+            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+               Neil Sims
+            </p>
+            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+               email@flowbite.com
+            </p>
+         </div>
+         <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+            $320
+         </div>
+      </div>
+   </li>
+   <li class="py-3 sm:py-4">
+      <div class="flex items-center space-x-4 rtl:space-x-reverse">
+         <div class="shrink-0">
+            <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Neil image">
+         </div>
+         <div class="flex-1 min-w-0">
+            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+               Bonnie Green
+            </p>
+            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+               email@flowbite.com
+            </p>
+         </div>
+         <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+            $3467
+         </div>
+      </div>
+   </li>
+   <li class="py-3 sm:py-4">
+      <div class="flex items-center space-x-4 rtl:space-x-reverse">
+         <div class="shrink-0">
+            <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Neil image">
+         </div>
+         <div class="flex-1 min-w-0">
+            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+               Michael Gough
+            </p>
+            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+               email@flowbite.com
+            </p>
+         </div>
+         <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+            $67
+         </div>
+      </div>
+   </li>
+   <li class="py-3 sm:py-4">
+      <div class="flex items-center space-x-4 rtl:space-x-reverse">
+         <div class="shrink-0">
+            <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Neil image">
+         </div>
+         <div class="flex-1 min-w-0">
+            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+               Thomas Lean
+            </p>
+            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+               email@flowbite.com
+            </p>
+         </div>
+         <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+            $2367
+         </div>
+      </div>
+   </li>
+   <li class="pt-3 pb-0 sm:pt-4">
+      <div class="flex items-center space-x-4 rtl:space-x-reverse">
+         <div class="shrink-0">
+            <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-4.jpg" alt="Neil image">
+         </div>
+         <div class="flex-1 min-w-0">
+            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+               Lana Byrd
+            </p>
+            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+               email@flowbite.com
+            </p>
+         </div>
+         <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+            $367
+         </div>
+      </div>
+   </li>
+</ul> */}
+
 
 
 export default Education;
--- src/shared-components/Award.js
+++ src/shared-components/Award.js
@@ -1,16 +1,34 @@
-const Award = ({awardName, awardDate}) => {
+const Award = ({awardName, awardDetails, awardDate}) => {
 
     return (
-        <li class="flex flex-col items-center space-x-3">
-            <svg class="w-12 h-12 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M11 9a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"/>
-                <path fill-rule="evenodd" d="M9.896 3.051a2.681 2.681 0 0 1 4.208 0c.147.186.38.282.615.255a2.681 2.681 0 0 1 2.976 2.975.681.681 0 0 0 .254.615 2.681 2.681 0 0 1 0 4.208.682.682 0 0 0-.254.615 2.681 2.681 0 0 1-2.976 2.976.681.681 0 0 0-.615.254 2.682 2.682 0 0 1-4.208 0 .681.681 0 0 0-.614-.255 2.681 2.681 0 0 1-2.976-2.975.681.681 0 0 0-.255-.615 2.681 2.681 0 0 1 0-4.208.681.681 0 0 0 .255-.615 2.681 2.681 0 0 1 2.976-2.975.681.681 0 0 0 .614-.255ZM12 6a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z" clip-rule="evenodd"/>
-                <path d="M5.395 15.055 4.07 19a1 1 0 0 0 1.264 1.267l1.95-.65 1.144 1.707A1 1 0 0 0 10.2 21.1l1.12-3.18a4.641 4.641 0 0 1-2.515-1.208 4.667 4.667 0 0 1-3.411-1.656Zm7.269 2.867 1.12 3.177a1 1 0 0 0 1.773.224l1.144-1.707 1.95.65A1 1 0 0 0 19.915 19l-1.32-3.93a4.667 4.667 0 0 1-3.4 1.642 4.643 4.643 0 0 1-2.53 1.21Z"/>
-            </svg>
-            <div>{awardName}</div>
-        </li>
+        <li class="pb-3 sm:pb-4 pt-3">
+        <div className="flex items-center space-x-4">
+            <div class="shrink-0">
+                <svg class="pt-3 w-12 h-12 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M11 9a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"/>
+                    <path fill-rule="evenodd" d="M9.896 3.051a2.681 2.681 0 0 1 4.208 0c.147.186.38.282.615.255a2.681 2.681 0 0 1 2.976 2.975.681.681 0 0 0 .254.615 2.681 2.681 0 0 1 0 4.208.682.682 0 0 0-.254.615 2.681 2.681 0 0 1-2.976 2.976.681.681 0 0 0-.615.254 2.682 2.682 0 0 1-4.208 0 .681.681 0 0 0-.614-.255 2.681 2.681 0 0 1-2.976-2.975.681.681 0 0 0-.255-.615 2.681 2.681 0 0 1 0-4.208.681.681 0 0 0 .255-.615 2.681 2.681 0 0 1 2.976-2.975.681.681 0 0 0 .614-.255ZM12 6a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z" clip-rule="evenodd"/>
+                    <path d="M5.395 15.055 4.07 19a1 1 0 0 0 1.264 1.267l1.95-.65 1.144 1.707A1 1 0 0 0 10.2 21.1l1.12-3.18a4.641 4.641 0 0 1-2.515-1.208 4.667 4.667 0 0 1-3.411-1.656Zm7.269 2.867 1.12 3.177a1 1 0 0 0 1.773.224l1.144-1.707 1.95.65A1 1 0 0 0 19.915 19l-1.32-3.93a4.667 4.667 0 0 1-3.4 1.642 4.643 4.643 0 0 1-2.53 1.21Z"/>
+                </svg>
+            </div>
+            <div class="flex-1 min-w-0">
+                <p class="text-sm font-medium text-gray-900 dark:text-white text-left">
+                    {awardName}
+                </p>
+                <p class="text-sm text-gray-500 dark:text-gray-400 text-left">
+                    {awardDetails}
+                </p>
+            </div>
+            <div class="max-w-4 inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                {awardDate}
+            </div>
+        </div>
+    </li>
     );
+
 }
+
+
+
 
 export default Award;
 
